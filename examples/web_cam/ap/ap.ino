60de1c125146934394f5ba32b40f862ce0a51713
Konstantin <69057532+archef2000@users.noreply.github.com>
2024-08-29 21:11:31 +0200

    Fix typo in ap.ino

--- examples/web_cam/ap/ap.ino
+++ examples/web_cam/ap/ap.ino
@@ -78,7 +78,7 @@ static const char* _STREAM_PART =
     "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 static void jpegStream(WiFiClient* client) {
-    Serial.println("Image stream satrt");
+    Serial.println("Image stream start");
     client->println("HTTP/1.1 200 OK");
     client->printf("Content-Type: %s\r\n", _STREAM_CONTENT_TYPE);
     client->println("Content-Disposition: inline; filename=capture.jpg");
